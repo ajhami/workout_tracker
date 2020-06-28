18d211164ae4d25bfc147981972a5f3a45ae7cfa
Alex Hamilton <alexhami916@gmail.com>
2020-06-27 20:52:26 -0600

    Updated schema and ran seed file.

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,38 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
     // A test variable to the workout schema
-    test: String
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    excerises: [{
+        type: {
+            type: String,
+            default: "resistance"
+        },
+        name: String,
+        duration:  {
+            type: Number,
+            default: 1
+        },
+        distance: {
+            type: Number,
+            default: 1
+        },
+        weight: {
+            type: Number,
+            default: 1
+        },
+        reps: {
+            type: Number,
+            default: 1
+        },
+        sets: {
+            type: Number,
+            default: 1
+        }
+    }]
+
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
