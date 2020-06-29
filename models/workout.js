ac6112f5d0915ba4b8d324ec715d59a585b439e9
Alex Hamilton <alexhami916@gmail.com>
2020-06-28 23:40:13 -0600

    Added remaining api routes. Tweeks to be made.

--- models/workout.js
+++ models/workout.js
@@ -1,60 +1,60 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    // A test variable to the workout schema
-    day: Date,
-    excerises: [{
-        type: {
-            type: {
-                type: String
-            }
-        },
-        name: String,
-        duration:  Number,
-        distance: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number
-    }]
-
-});
-
 // const workoutSchema = new Schema({
 //     // A test variable to the workout schema
-//     day: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     excerises: [{
+//     day: Date,
+//     exercises: [{
 //         type: {
-//             type: String,
-//             default: "resistance"
+//             type: {
+//                 type: String
+//             }
 //         },
 //         name: String,
-//         duration:  {
-//             type: Number,
-//             default: 1
-//         },
-//         distance: {
-//             type: Number,
-//             default: 1
-//         },
-//         weight: {
-//             type: Number,
-//             default: 1
-//         },
-//         reps: {
-//             type: Number,
-//             default: 1
-//         },
-//         sets: {
-//             type: Number,
-//             default: 1
-//         }
+//         duration:  Number,
+//         distance: Number,
+//         weight: Number,
+//         reps: Number,
+//         sets: Number
 //     }]
 
 // });
+
+const workoutSchema = new Schema({
+    // A test variable to the workout schema
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            default: "resistance"
+        },
+        name: String,
+        duration:  {
+            type: Number,
+            default: 1
+        },
+        distance: {
+            type: Number,
+            default: 1
+        },
+        weight: {
+            type: Number,
+            default: 1
+        },
+        reps: {
+            type: Number,
+            default: 1
+        },
+        sets: {
+            type: Number,
+            default: 1
+        }
+    }]
+
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const db = require("./../models");
-
+// const mongojs = require("mongojs");
 
 // ADD API ROUTES HERE
 
@@ -14,8 +14,85 @@ router.get("/api/workouts", (req, res) => {
         .catch(err => {
             res.status(400).json(err);
         });
-})
+});
 
+router.post("/api/workouts", (req, res) => {
+    console.log("creating new workout");
 
+    db.Workout.create({
+        // day: Date.now(),
+        // exercises: []
+    })
+        .then(newWorkout => {
+            res.json(newWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    console.log("PUT ROUTE REACHED!");
+    console.log(req.params.id);
+    console.log(req.body);
+    console.log(req.body.type);
+    if (req.body.type === "resistance") {
+        db.Workout.update({
+            // "_id": mongojs.ObjectId(req.params.id)
+            "_id": req.params.id
+        },
+        {
+            "$set": {
+                "exercises.$type": req.body.type,
+                "exercises.$name": req.body.name,
+                "exercises.$duration": req.body.duration,
+                "exercises.$weight": req.body.weight,
+                "exercises.$reps": req.body.reps,
+                "exercises.$sets": req.body.sets
+            }
+        })
+            .then(function (addedWorkout) {
+                res.json(addedWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    } else if (req.body.type === "cardio") {
+        db.Workout.update({
+            // "_id": mongojs.ObjectId(req.params.id)
+            "_id": req.params.id
+        },
+        {
+            "$set": {
+                "exercises.$type": req.body.type,
+                "exercises.$name": req.body.name,
+                "exercises.$duration": req.body.duration,
+                "exercises.$distance": req.body.distance
+            }
+        })
+            .then(function (addedWorkout) {
+                res.json(addedWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    }
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    console.log("/api/workouts/range found!");
+    db.Workout.find({
+        "day": {
+            "$gte": new Date().setDate(new Date().getDate()-7),
+            "$lt": new Date().setDate(new Date().getDate())
+        }
+    })
+        .then(recentWorkouts => {
+            res.json(recentWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
